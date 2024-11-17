53765e07e740bce677a62f4ab76244552bb28086
Rayhan Naeem <a.r.naeem786@gmail.com>
2024-11-17 18:50:11 +0600

    add files to github

---
+++ basicReact/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+//import Title from "./Title";
+import ProductTab from "./Producttab";
+
+function App() {
+  return <ProductTab />;
+}
+
+export default App;
---
+++ basicReact/src/Title.jsx
@@ -0,0 +1,12 @@
+function Title() {
+  let name = "naeem";
+  return (
+    <div>
+      <h1>I am the title</h1>
+      <p>4*6 = {4 * 6}</p>
+      <p>I am {name.toUpperCase()}</p>
+    </div>
+  );
+}
+
+export default Title;
